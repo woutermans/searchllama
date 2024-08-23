87a23ade9983ddc41137d0eb2bcbb3c7a037771a
Your Name <you@example.com>
2024-08-23 15:20:40 +0200

    feat: update dependencies and simplify router usage

--- searchllama-web/src/main.rs
+++ searchllama-web/src/main.rs
@@ -1,5 +1,4 @@
 use yew::prelude::*;
-use yew_router::prelude::*;
 
 struct Model {
     value: i64,
@@ -7,24 +6,15 @@ struct Model {
 
 enum Msg {
     AddOne,
-    SetValue(i64),
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
-        let mut value = 0;
-        if let Some(query) = ctx.link().location().query() {
-            if let Some(q_value) = query.get("q") {
-                if let Ok(parsed_value) = q_value.parse::<i64>() {
-                    value = parsed_value;
-                }
-            }
-        }
+    fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            value,
+            value: 0,
         }
     }
 
@@ -32,10 +22,6 @@ impl Component for Model {
         match msg {
             Msg::AddOne => {
                 self.value += 1;
-                true
-            }
-            Msg::SetValue(new_value) => {
-                self.value = new_value;
                 true
             }
         }
@@ -52,24 +38,10 @@ impl Component for Model {
     }
 }
 
-#[derive(Clone, Routable, PartialEq)]
-enum Route {
-    #[at("/")]
-    Home,
-}
-
-fn switch(routes: &Route) -> Html {
-    match routes {
-        Route::Home => html! { <Model /> },
-    }
-}
-
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
+        <Model />
     }
 }
 
